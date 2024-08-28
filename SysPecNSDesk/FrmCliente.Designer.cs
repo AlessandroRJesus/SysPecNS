e084c82cd564d6ce781f03902a56cbc6ae988833
ALESSANDRO RODRIGUES DE JESUS <nandok1012@gmail.com>
2024-08-28 11:54:14 -0300

    frm do cliente

---
+++ SysPecNSDesk/FrmCliente.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SysPecNSDesk
+{
+    partial class FrmCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbl_ID = new Label();
+            lbl_Email = new Label();
+            lbl_CPF = new Label();
+            button1 = new Button();
+            lbl_Nome = new Label();
+            lbl_DataNasc = new Label();
+            lbl_DataCod = new Label();
+            lbl_Ativo = new Label();
+            SuspendLayout();
+            // 
+            // lbl_ID
+            // 
+            lbl_ID.AutoSize = true;
+            lbl_ID.Location = new Point(49, 50);
+            lbl_ID.Name = "lbl_ID";
+            lbl_ID.Size = new Size(18, 15);
+            lbl_ID.TabIndex = 0;
+            lbl_ID.Text = "ID";
+            // 
+            // lbl_Email
+            // 
+            lbl_Email.AutoSize = true;
+            lbl_Email.Location = new Point(111, 46);
+            lbl_Email.Name = "lbl_Email";
+            lbl_Email.Size = new Size(36, 15);
+            lbl_Email.TabIndex = 1;
+            lbl_Email.Text = "E&mail";
+            // 
+            // lbl_CPF
+            // 
+            lbl_CPF.AutoSize = true;
+            lbl_CPF.Location = new Point(193, 48);
+            lbl_CPF.Name = "lbl_CPF";
+            lbl_CPF.Size = new Size(28, 15);
+            lbl_CPF.TabIndex = 2;
+            lbl_CPF.Text = "CPF";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(198, 226);
+            button1.Name = "button1";
+            button1.Size = new Size(71, 40);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // lbl_Nome
+            // 
+            lbl_Nome.AutoSize = true;
+            lbl_Nome.Location = new Point(277, 39);
+            lbl_Nome.Name = "lbl_Nome";
+            lbl_Nome.Size = new Size(40, 15);
+            lbl_Nome.TabIndex = 4;
+            lbl_Nome.Text = "Nome";
+            // 
+            // lbl_DataNasc
+            // 
+            lbl_DataNasc.AutoSize = true;
+            lbl_DataNasc.Location = new Point(361, 58);
+            lbl_DataNasc.Name = "lbl_DataNasc";
+            lbl_DataNasc.Size = new Size(112, 15);
+            lbl_DataNasc.TabIndex = 5;
+            lbl_DataNasc.Text = "Data de nascimento";
+            // 
+            // lbl_DataCod
+            // 
+            lbl_DataCod.AutoSize = true;
+            lbl_DataCod.Location = new Point(434, 94);
+            lbl_DataCod.Name = "lbl_DataCod";
+            lbl_DataCod.Size = new Size(98, 15);
+            lbl_DataCod.TabIndex = 6;
+            lbl_DataCod.Text = "Data De Cadastro";
+            // 
+            // lbl_Ativo
+            // 
+            lbl_Ativo.AutoSize = true;
+            lbl_Ativo.Location = new Point(526, 125);
+            lbl_Ativo.Name = "lbl_Ativo";
+            lbl_Ativo.Size = new Size(35, 15);
+            lbl_Ativo.TabIndex = 7;
+            lbl_Ativo.Text = "Ativo";
+            // 
+            // FrmCliente
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lbl_Ativo);
+            Controls.Add(lbl_DataCod);
+            Controls.Add(lbl_DataNasc);
+            Controls.Add(lbl_Nome);
+            Controls.Add(button1);
+            Controls.Add(lbl_CPF);
+            Controls.Add(lbl_Email);
+            Controls.Add(lbl_ID);
+            Name = "FrmCliente";
+            Text = "FrmCliente";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbl_ID;
+        private Label lbl_Email;
+        private Label lbl_CPF;
+        private Button button1;
+        private Label lbl_Nome;
+        private Label lbl_DataNasc;
+        private Label lbl_DataCod;
+        private Label lbl_Ativo;
+    }
+}
--- SysPecNSLib/Cliente.cs
+++ SysPecNSLib/Cliente.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,19 +14,27 @@ namespace SysPecNSLib
         public string? Nome { get; set; }
         public string? Cpf { get; set; }
         public string? Telefone { get; set; }
-        public string? Email  { get; set; }
-        public DateTime? Data_nasc{ get; set; }
-        public DateTime? Data_cad {  get; set; }
-        public string? Ativo { get; set; }
+        public string? Email { get; set; }
+        public DateTime? Data_nasc { get; set; }
+        public DateTime? Data_cad { get; set; }
+        public bool Ativo { get; set; }
 
 
         public Cliente()
         {
-         
+            Id = 0;
         }
 
-
-        public Cliente(string? nome, string? cpf, string? telefone, string? email, DateTime? data_nasc, DateTime? data_cad, string? ativo)
+        public Cliente(string? nome, string? cpf, string? telefone, string? email, DateTime? data_nasc, DateTime? data_cad)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Telefone = telefone;
+            Email = email;
+            Data_nasc = data_nasc;
+            Data_cad = data_cad;
+        }
+        public Cliente(string? nome, string? cpf, string? telefone, string? email, DateTime? data_nasc, DateTime? data_cad, bool ativo)
         {
             Nome = nome;
             Cpf = cpf;
@@ -35,9 +45,9 @@ namespace SysPecNSLib
             Ativo = ativo;
         }
 
-        public Cliente (int id,string? nome, string? cpf, string? telefone, string? email, DateTime? data_nasc, DateTime? data_cad, string? ativo)
+        public Cliente(int id, string? nome, string? cpf, string? telefone, string? email, DateTime? data_nasc, DateTime? data_cad, bool ativo)
         {
-            Id = Id;
+            Id = id;
             Nome = nome;
             Cpf = cpf;
             Telefone = telefone;
@@ -46,8 +56,91 @@ namespace SysPecNSLib
             Data_cad = data_cad;
             Ativo = ativo;
         }
+        public void Inserir()
+        {
+            //Inserir os clientes no Banco de dados 
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_cliente_insert";
+            cmd.Parameters.AddWithValue("spnome", Nome);
+            cmd.Parameters.AddWithValue("spemail", Email);
+            cmd.Parameters.AddWithValue("spcpf", Cpf);
+            cmd.Parameters.AddWithValue("sptelefone", Telefone);
+            cmd.Parameters.AddWithValue("spdatanasc", Data_nasc);
+            cmd.Parameters.AddWithValue("spdatacad", Data_cad);
+            cmd.Parameters.AddWithValue("spativo", Ativo);
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Id = dr.GetInt32(0);
+            }
+        }
 
+
+        public static Cliente ObterPorId(int id)
+
+        {
+            //puxar o cliente do Banco de dados pelo ID
+            Cliente cliente = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"select *from cliente where id = {id}";
+            var dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                cliente = new(
+                         dr.GetInt32(0),
+                         dr.GetString(1),
+                         dr.GetString(2),
+                         dr.GetString(3),
+                         dr.GetString(4),
+                         dr.GetDateTime(5),
+                         dr.GetDateTime(6),
+                         dr.GetBoolean(7)
+                         );
+            }
+            return cliente;
+
+
+
+        }
+        public static List<Cliente> ObterLista(string? nome = "")
+        {
+            // lista de cliente
+            List<Cliente> lista = new();
+            var comandosSQL = Banco.Abrir();
+            comandosSQL.CommandType = CommandType.Text;
+            if (nome == "")
+            {
+                comandosSQL.CommandText = "select *from cliente order by nome limit 10";
+            }
+            else
+            {
+                comandosSQL.CommandText = $"select *from cliente where nome like '%{nome}%' order by nome limit 10";
+            }
+
+
+            var dr = comandosSQL.ExecuteReader();
+            while (dr.Read())
+            {
+                lista.Add(
+                    new(
+                     dr.GetInt32(0),
+                     dr.GetString(1),
+                     dr.GetString(2),
+                     dr.GetString(3),
+                     dr.GetString(4),
+                     dr.GetDateTime(5),
+                     dr.GetDateTime(6),
+                     dr.GetBoolean(7)
+                      )
+                    );
+            }
+
+            return lista;
+        }
+
+   
 
     }
-
 }
